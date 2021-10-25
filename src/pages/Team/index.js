d3b94d4ed34806e669ae74a435c9b288455a4630
joker49 <linhmk2905@gmail.com>
2021-10-25 08:56:04 +0700

    update code

--- src/pages/Team/index.js
+++ src/pages/Team/index.js
@@ -1,4 +1,56 @@
 export default function Team(){
+
+    var list1 = [
+      {
+        id: '1',
+        name: 'Trần Nghĩa',
+        title: 'Founder &amp; Creative Front-End Developer',
+      },
+      {
+        id: '2',
+        name: 'Đặng Thuyền Vương',
+        title: 'Co-Founder &amp; Fullstack Developer',
+      },
+      {
+        id: '3',
+        name: 'Đặng Thuyền Quân',
+        title: 'Co-Founder &amp; Backend Developer',
+      },
+      {
+        id: '4',
+        name: 'Nguyễn Đức Huy',
+        title: 'Co-Founder &amp; Front-End Developer',
+      },
+    ];
+
+    var list2 = [
+      {
+        id: '1',
+        name: 'Phạm Thành Trung',
+        title: 'CFD1-Offline'
+      },
+      {
+        id: '2',
+        name: 'Ngô Thành Long',
+        title: 'CFD1-Offline'
+      },
+      {
+        id: '3',
+        name: 'Huỳnh Tiến Tài',
+        title: 'CFD1-Offline'
+      },
+      {
+        id: '4',
+        name: 'Diệp Anh Thy',
+        title: 'CFD1-Offline'
+      },
+      {
+        id: '5',
+        name: 'Trần Anh Tuấn',
+        title: 'CFD1-Offline'
+      },
+    ]
+
     return(
         <main className="team" id="main">
         <section>
@@ -11,150 +63,41 @@ export default function Team(){
               </p>
             </div>
             <div className="list row">
-              <div className="item col-md-6 col-sm-6">
-                <div className="wrap">
-                  <div className="cover">
-                    <img src="img/thumb-member.jpg" alt="" />
-                  </div>
-                  <div className="info">
-                    <div className="name">
-                      trần nghĩa
+              {
+                list1.map(o =>(
+                  <div className="item col-md-6 col-sm-6">
+                    <div className="wrap">
+                      <div className="cover">
+                        <img src="img/thumb-member.jpg" alt="" />
+                      </div>
+                      <div className="info">
+                        <div className="name">
+                          {o.name}
+                        </div>
+                        <p className="title">{o.title}</p>
+                      </div>
                     </div>
-                    <p className="title">Founder &amp; Creative Front-End Developer</p>
                   </div>
-                </div>
-              </div>
-              <div className="item col-md-6 col-sm-6">
-                <div className="wrap">
-                  <div className="cover">
-                    <img src="img/thumb-member.jpg" alt="" />
-                  </div>
-                  <div className="info">
-                    <div className="name">
-                      đặng thuyền vương
+                ))
+              }
+              {
+                list2.map(o => (
+                  <div className="item col-md-3 col-sm-4 col-xs-6">
+                    <div className="wrap">
+                      <div className="cover">
+                        <span className="text">H</span>
+                        <img src="img/thumb-member.jpg" alt="" />
+                      </div>
+                      <div className="info">
+                        <div className="name">
+                          {o.name}
+                        </div>
+                        <p className="title">{o.title}</p>
+                      </div>
                     </div>
-                    <p className="title">Co-Founder &amp; Fullstack Developer</p>
                   </div>
-                </div>
-              </div>
-              <div className="item col-md-6 col-sm-6">
-                <div className="wrap">
-                  <div className="cover">
-                    <img src="img/thumb-member.jpg" alt="" />
-                  </div>
-                  <div className="info">
-                    <div className="name">
-                      đặng thuyền quân
-                    </div>
-                    <p className="title">Co-Founder &amp; Backend Developer</p>
-                  </div>
-                </div>
-              </div>
-              <div className="item col-md-6 col-sm-6">
-                <div className="wrap">
-                  <div className="cover">
-                    <img src="img/thumb-member.jpg" alt="" />
-                  </div>
-                  <div className="info">
-                    <div className="name">
-                      nguyễn đức huy
-                    </div>
-                    <p className="title">Co-Founder &amp; Front-End Developer</p>
-                  </div>
-                </div>
-              </div>
-              <div className="item col-md-3 col-sm-4 col-xs-6">
-                <div className="wrap">
-                  <div className="cover">
-                    <span className="text">H</span>
-                    <img src="img/thumb-member.jpg" alt="" />
-                  </div>
-                  <div className="info">
-                    <div className="name">
-                      Diệp anh thy
-                    </div>
-                    <p className="title">CFD1-Offline</p>
-                  </div>
-                </div>
-              </div>
-              <div className="item col-md-3 col-sm-4 col-xs-6">
-                <div className="wrap">
-                  <div className="cover">
-                    <img src="img/thumb-member.jpg" alt="" />
-                  </div>
-                  <div className="info">
-                    <div className="name">
-                      huỳnh tiến tài
-                    </div>
-                    <p className="title">CFD1-Offline</p>
-                  </div>
-                </div>
-              </div>
-              <div className="item col-md-3 col-sm-4 col-xs-6">
-                <div className="wrap">
-                  <div className="cover">
-                    <img src="img/thumb-member.jpg" alt="" />
-                  </div>
-                  <div className="info">
-                    <div className="name">
-                      Ngô thành long
-                    </div>
-                    <p className="title">CFD1-Offline</p>
-                  </div>
-                </div>
-              </div>
-              <div className="item col-md-3 col-sm-4 col-xs-6">
-                <div className="wrap">
-                  <div className="cover">
-                    <img src="img/thumb-member.jpg" alt="" />
-                  </div>
-                  <div className="info">
-                    <div className="name">
-                      trần anh tuấn
-                    </div>
-                    <p className="title">CFD1-Offline</p>
-                  </div>
-                </div>
-              </div>
-              <div className="item col-md-3 col-sm-4 col-xs-6">
-                <div className="wrap">
-                  <div className="cover">
-                    <img src="img/thumb-member.jpg" alt="" />
-                  </div>
-                  <div className="info">
-                    <div className="name">
-                      ngô thành long
-                    </div>
-                    <p className="title">CFD1-Offline</p>
-                  </div>
-                </div>
-              </div>
-              <div className="item col-md-3 col-sm-4 col-xs-6">
-                <div className="wrap">
-                  <div className="cover">
-                    <img src="img/thumb-member.jpg" alt="" />
-                  </div>
-                  <div className="info">
-                    <div className="name">
-                      nguyễn thành tùng
-                    </div>
-                    <p className="title">CFD1-Offline</p>
-                  </div>
-                </div>
-              </div>
-              <div className="item col-md-3 col-sm-4 col-xs-6">
-                <div className="wrap">
-                  <div className="cover">
-                    <img src="img/thumb-member.jpg" alt="" />
-                  </div>
-                  <div className="info">
-                    <div className="name">
-                      phạm thành trung
-                    </div>
-                    <p className="title">CFD1-Offline</p>
-                  </div>
-                </div>
-              </div>
+                ))
+              }
             </div>
           </div>
         </section>
